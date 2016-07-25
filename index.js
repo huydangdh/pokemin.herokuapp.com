2c912a2045733f15d5e4a38c973d8a0e7ffeff16
huydangdh <huydang.dh@mt2015.com>
2016-07-25 14:00:49 +0700

    Make It Better.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var app = express();
 var bodyParser = require('body-parser');
 var compression = require('compression');
 var fs = require('fs');
-var algorithmia = require("algorithmia");
 var request = require('request');
+var mime = require('mime');
 var zlib = require("zlib");
-
+var date = new Date();
 var url = ["http://ouo.io/I1Mzz", "http://ouo.io/CsuQIS"];
 
 app.set('port', (process.env.PORT || 5000));
@@ -16,15 +16,16 @@ app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('/download', function (req, res) {
+    console.log("[GET] ", req.query.file, date.getTime());
     var query = req.query.file;
-    var referer = req.header('Referer');
+    var referer = req.headers.referer;
 
     if (referer.indexOf("ouo.io") > -1) {
-        
+
         if (query === "POKEMON_GO_MOD.APK" || query === "HUONG_DAN.RAR") {
 
             var file = __dirname + '/upload/' + query;
-            var filename = path.basename(file);
+            var filename = query;
             var mimetype = mime.lookup(file);
 
             res.setHeader('Content-disposition', 'attachment; filename=' + filename);
